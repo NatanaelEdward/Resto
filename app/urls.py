c079ce038f73b4ea8f1d76a9cc56eba173dee6d5
NatanaelEdward <112832301+NatanaelEdward@users.noreply.github.com>
2023-09-09 09:01:40 +0700

    simple logout

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_view,index
+from .views import login_view,index,logout_view
 
 urlpatterns =[
     path('', login_view, name='login_view'),
+    path('logout/', logout_view, name='logout_view'),
     path('index', index,name='index'), 
 ]
--- app/views.py
+++ app/views.py
@@ -17,6 +17,10 @@ def login_view(request):
             return render(request, 'login.html', {'error': 'Invalid login credentials.'})
     else:
         return render(request, 'login.html')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
 
 def index(request):
     return render(request, 'index.html')
