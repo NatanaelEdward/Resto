685913d44104a634227060b3d76cde4595f54ec4
NatanaelEdward <112832301+NatanaelEdward@users.noreply.github.com>
2023-09-10 16:21:12 +0700

    deleted comment

--- app/views.py
+++ app/views.py
@@ -4,21 +4,6 @@ from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 import json
 
-# Create your views here.
-# def login_view(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return render(request,'Kasir/index.html')
-#         else:
-#             return render(request, 'login.html', {'error': 'Invalid login credentials.'})
-#     else:
-#         return render(request, 'login.html')
-
-#test login
 
 def login_view(request):
     if request.method == 'POST':
