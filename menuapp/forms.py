a0641180e9d182de4328ccedc7628826dbda1ca7
NatanaelEdward <112832301+NatanaelEdward@users.noreply.github.com>
2023-10-06 16:21:19 +0700

    import rndom

--- menuapp/forms.py
+++ menuapp/forms.py
@@ -6,3 +6,4 @@ class DataMenuForm(forms.ModelForm):
     class Meta:
         model = DataMenu
         fields = '__all__'  # Atau tentukan bidang apa saja yang ingin Anda tampilkan dalam form.
+
--- menuapp/views.py
+++ menuapp/views.py
@@ -1,9 +1,11 @@
 # menuapp/views.py
 import json
+import random
 from django.shortcuts import render, redirect
 from .forms import DataMenuForm
 from django.http import JsonResponse
 from .models import DataMenu, JenisMenu,PenjualanDetail,HargaMenu,JenisSize
+from decimal import Decimal
 
 def index_makanan(request):
     kategori = request.GET.get('kategori', '')  # Mendapatkan nilai parameter kategori dari URL
