43a8559998992eb6f8cf35eeba0524327673479f
NatanaelEdward <112832301+NatanaelEdward@users.noreply.github.com>
2023-10-10 16:26:40 +0700

    generate pdf

--- kasirapp/urls.py
+++ kasirapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import kasiran,pesanan,tabelKasir,update_order,cancel_order,delete_order
+from .views import kasiran,pesanan,tabelKasir,update_order,cancel_order,delete_order,generate_pdf
 
 urlpatterns = [
     path('kasiran', kasiran, name='kasiran'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('update_order/<int:order_id>/',update_order, name='update_order'),
     path('cancel_order/<int:order_id>/',cancel_order, name='cancel_order'),
     path('delete_order/<int:order_id>/',delete_order, name='delete_order'),
+    path('generate_pdf/<int:order_id>/',generate_pdf, name='generate_pdf'),
+
 
 ]
--- kasirapp/views.py
+++ kasirapp/views.py
@@ -1,13 +1,67 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.http import FileResponse
+from io import BytesIO
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.pdfgen import canvas
 from menuapp.models import PenjualanDetail,PenjualanFaktur
 from django.contrib.auth.decorators import login_required
 import requests
 from decimal import Decimal
+from django.http import HttpResponse
 from django.http import JsonResponse
 
 # Create your views here.
+def generate_pdf(request, order_id):
+    # Get the PenjualanFaktur instance
+    order = get_object_or_404(PenjualanFaktur, id=order_id)
+
+    # Create a BytesIO buffer to receive the PDF data
+    buffer = BytesIO()
+
+    # Create the PDF object, using the BytesIO buffer as its "file."
+    p = canvas.Canvas(buffer, pagesize=landscape(letter))
+
+    # Set a large enough page size
+    width, height = landscape(letter)
+    p.setPageSize((width, height))
+
+    # Define content to print
+    content = [
+        f'FAKTUR PENJUALAN',
+        f'Nomor Nota Penjualan: {order.kode_penjualan_faktur}',
+        f'Meja: {order.nomor_meja}',
+        f'Tanggal Penjualan: {order.tanggal_penjualan.strftime("%b. %d, %Y, %I:%M %p")}',
+        f'Total Penjualan: {order.total_penjualan}',
+        f'Total Pembayaran : {order.pembayaran}',
+        f'Kembalian : {order.kembalian}',
+        f'Status Lunas : {"Lunas" if order.status_lunas else "Belum Lunas"}'
+    ]
+
+    # Set the Y-coordinate for the first line of content
+    y = height - 100
+
+    # Add content to the PDF
+    for line in content:
+        p.drawString(100, y, line)
+        y -= 20  # Adjust the Y-coordinate for the next line
+
+    # Close the PDF object cleanly and finalize the buffer.
+    p.showPage()
+    p.save()
+
+    # FileResponse to send the PDF as a response
+    pdf_data = buffer.getvalue()
+    buffer.close()
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="invoice_{order.kode_penjualan_faktur}.pdf"'
+    response.write(pdf_data)
+
+    return response
+
+
+
 
 @login_required
 def kasiran(request):
