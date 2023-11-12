e3ca23abe4d8627607748c738b834c8da15ac5ae
ofelito <ofelito0301@gmail.com>
2023-11-12 22:13:13 +0700

    add interface

--- resto/settings.py
+++ resto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qj+$le7k@trs3ipb!ch=yk=))opwb)9l8_#nh0$_labh=*ktwp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
